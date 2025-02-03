07fb03f4f0049df592282eb8080691825342f6d5
WestraTech <westratechnology@gmail.com>
2025-02-03 11:06:15 -0500

    Learning How Numbers Work

---
+++ number.py
@@ -0,0 +1,8 @@
+num1 = 1
+num2 = 1.25
+num3 = "10"
+
+print( type(num1) ) #Integer
+print( type(num2) ) #Float
+print( num3 + "1" ) #Will print 10 and 1 together
+print( int(num3) + 1 ) #Will print 10 + 1
---
+++ number2.py
@@ -0,0 +1,7 @@
+print( 10 - 5 ) #Subtraction
+print( 10 + 5 ) #Addition
+print( 10 * 5 ) #Multiplication
+print( 10 / 5 ) #Division
+print( 10 ** 5 ) #Exponential
+print( 10 // 5 ) #Flow Division
+print( 10 % 5 ) #Remainder
